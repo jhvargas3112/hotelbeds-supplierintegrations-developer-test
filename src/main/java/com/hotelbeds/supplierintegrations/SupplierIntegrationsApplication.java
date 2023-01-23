0430d5052638d4681bdf0752011a8343e76e1634
Jhonny Vargas Paredes <jhvargas3112@gmail.com>
2023-01-23 23:53:39 +0100

    created maven project

---
+++ src/main/java/com/hotelbeds/supplierintegrations/SupplierIntegrationsApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbeds.supplierintegrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierIntegrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierIntegrationsApplication.class, args);
+	}
+
+}
